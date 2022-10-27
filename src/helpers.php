3f385554da1a8ddf9bfb3deba3998afc0a55949e
Douwe de Haan <rinzedouwedehaan@gmail.com>
2022-10-27 16:51:44 +0200

    Added the view function with some small exception handling

--- src/helpers.php
+++ src/helpers.php
@@ -4,3 +4,25 @@ function hl( $file, $syntax )
 {
     return \TrafficSupply\Documentation\Highlighter::file( $file, $syntax );
 }
+
+function view($name, $data) {
+
+    $file_path = __DIR__.'/views/'.$name.'.php';
+
+    if ( ! is_file($file_path) ) {
+        throw new Exception('View '.$name.' not found');
+    }
+
+    ob_start();
+
+    foreach ( $data as $variable_name => $value ) {
+        $$variable_name = $value;
+    }
+
+    include ( $file_path );
+
+    $content = ob_get_contents();
+    ob_end_clean();
+
+    return $content;
+}
